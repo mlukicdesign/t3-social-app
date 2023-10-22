ce74f8598e0033161531193f0821cf323ebf01a3
mlukicdesign <mlukicdesign@gmail.com>
2023-10-22 18:44:58 +0800

    started navigation components

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+import { signOut, signIn, useSession } from 'next-auth/react'
+
+
+const SideNav = () => {
+
+    const session = useSession()
+
+    const user = session.data?.user
+
+  return (
+    <nav className="sticky top-0 self-start px-2 py-4">
+        <ul className='flex flex-col items-start gap-2 whitespace-nowrap'>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            {/* Only show is user is logged in */}
+            {/* Use user session to concatenate the user's id to the profile path */}
+            {user != null && (
+                <li>
+                    <Link href={`/profiles/${user.id}`}>Profile</Link>
+                </li>
+            )}
+            {user == null ? (
+                <li>
+                    <button onClick={() => void signOut()}>Log Out</button>
+                </li>
+            ) : <li>
+                <button onClick={() => void signIn()}>Log In</button>
+            </li> } 
+            
+        </ul>
+    </nav>
+  )
+}
+
+export default SideNav
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,12 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
-import { api } from "~/utils/api";
+
+import { api } from "~/utils/api"; 
+import SideNav from "~/components/SideNav";
+
+import Head from "next/head";
+
 
 import "~/styles/globals.css";
 
@@ -12,7 +17,18 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>T3 Social App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <meta name="description" content="A social media app created with the T3 stack"></meta>
+      <div className="container mx-auto flex">
+      <SideNav />
+      <div className="min-h-screen flex-grow border-x">
+
+      </div>
       <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
